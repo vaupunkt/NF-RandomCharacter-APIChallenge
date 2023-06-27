e76e490214bbc26666e23a4594a860f753274f2c
Veliko <Kardziev>
2023-06-27 15:49:34 +0200

    solve challenge-tasks

---
+++ pages/api/random-character.js
@@ -0,0 +1,12 @@
+import Chance from "chance";
+
+export default function handler(request, response) {
+  const chance = new Chance();
+  const character = {
+    firstName: chance.first(),
+    lastName: chance.last(),
+    twitterName: chance.twitter(),
+    geohash: chance.geohash(),
+  };
+  response.status(200).json(character);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
+import useSWR from "swr";
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR("/api/random-character", fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+  return (
+    <section>
+      <h2>Hello, I am {data.firstName}</h2>
+      <p>
+        <strong>Name:</strong> {data.firstName} {data.lastName}
+      </p>
+      <p>
+        <strong>Twitter-Handle:</strong> {data.twitterName}
+      </p>
+      <p>
+        <strong>Geohash:</strong> {data.geohash}
+      </p>
+    </section>
+  );
 }
